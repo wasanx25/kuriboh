49782687adfdab9bd8fea7f4db5b627f63634bc2
wasanx25 <wasanx25@gmail.com>
2019-06-25 01:02:03 +0900

    Add provider

---
+++ src/main/kotlin/com/github/wasanx25/KuribohProvider.kt
@@ -0,0 +1,15 @@
+package com.github.wasanx25
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+import com.github.wasanx25.rules.CallPrintln
+
+class KuribohProvider : RuleSetProvider {
+    override val ruleSetId: String = "sample"
+
+    override fun instance(config: Config): RuleSet {
+        return RuleSet(ruleSetId, listOf(
+            CallPrintln()
+        ))
+    }
+}
