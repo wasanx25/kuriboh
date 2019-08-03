11b9f9be989b4b30a2e7e23e856d0b163e06efcb
wasanx25 <wasanx25@gmail.com>
2019-08-03 10:11:54 +0900

    Changes detekt rule name

--- src/main/kotlin/com/github/wasanx25/KuribohProvider.kt
+++ src/main/kotlin/com/github/wasanx25/KuribohProvider.kt
@@ -5,7 +5,7 @@ import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 import com.github.wasanx25.rules.UselessCallFunction
 
 class KuribohProvider : RuleSetProvider {
-    override val ruleSetId: String = "sample"
+    override val ruleSetId: String = "style"
 
     override fun instance(config: Config): RuleSet {
         return RuleSet(ruleSetId, listOf(
